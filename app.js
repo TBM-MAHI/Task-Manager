7d0012439d4f6e857d3e10699efae5bad97b69f5
TBM-MAHI <tasnimul.bin.moshiur@g.bracu.ac.bd>
2022-04-28 16:26:16 +0600

    modified

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/conect.js'); 
 require('dotenv').config()
-
+const notFound=require('./middleware/not-found')
 const app = express();
 //middleware
 app.use(express.static('view'))
 app.use(express.json());
 //routes
-
 app.use('/tasks', tasks); //Mounting the middleware functions (path, MW func)
                         // the path is the default route replacing "/"
+app.use(notFound);
 
 const port = 3000;
 async function start() {
@@ -23,5 +23,4 @@ try {
      console.log(error);
     }
 }
-//comment jhjurh
-start();
+start()
