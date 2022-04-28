810b633572adb5f20937a59f33f2ccac40921153
TBM-MAHI <tasnimul.bin.moshiur@g.bracu.ac.bd>
2022-04-28 16:43:16 +0600

    mod

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router(); //router object is an isolated instance of middleware and routes.
 
 const { createTasks, deleteTask, getTask, getallTasks, updateTask } = require('../controllers/tasks');
-const { create_newUser, VerifyUser } = require('../controllers/users');
+const { create_newUser, VerifyUser} = require('../controllers/users');
 //console.log(typeof(deleteTask));
 
-router.route('/all').get(getallTasks).post(createTasks);  //the default route 
+router.route('/all/:UID').get(getallTasks);  //the default route 
+router.route('/all').post(createTasks);
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 router.route('/register').post(create_newUser);
 router.route('/login/:name/:pass').get(VerifyUser);
